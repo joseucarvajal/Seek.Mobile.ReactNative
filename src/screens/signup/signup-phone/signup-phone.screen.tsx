6847831d2e8d372a2dfd8152f75c39eec5443508
joseucarvajal <joseucarvajal@gmail.com>
2020-10-20 09:01:17 -0500

    Sign up phone form

--- src/screens/signup/signup-phone/signup-phone.screen.tsx
+++ src/screens/signup/signup-phone/signup-phone.screen.tsx
@@ -2,20 +2,13 @@ import React from "react";
 import { StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-import DropDownPicker from 'react-native-dropdown-picker';
-
+import DropDownPicker from "react-native-dropdown-picker";
 
 import { useNavigation } from "@react-navigation/native";
 
-import {
-  Button,
-  ButtonTertiary,
-  Text,
-  Select,
-  Input,
-} from "../../../shared";
+import { Button, ButtonTertiary, Text, Select, Input } from "../../../shared";
 
-import { Colors, FontNames, getFontStyle } from "../../../constants";
+import { Colors, FontNames, getFontStyle, Layout } from "../../../constants";
 
 import { signUpStyles, SeekQLogo } from "../../../components/signup/";
 
@@ -44,12 +37,24 @@ const SignUpPhone: React.FC<ISignUpPhoneProps> = ({}) => {
         </Text>
 
         <View style={styles.phoneForm}>
-          <Select
-            defaultValue={"1"}
-            items={[{ label: "1", value: "1" }]}    
-            style={styles.phoneIndicative}
-          />
-          <Input color="quaternary" style={styles.phoneNumber} />
+          <View style={styles.phoneIndicativeView}>
+            <Select
+              defaultValue={"1"}
+              items={[{ label: "1", value: "1" }]}
+              style={styles.phoneIndicative}
+            />
+          </View>
+          <View style={styles.phoneNumberView}>
+            <Input
+              color="primary"
+              style={styles.phoneNumber}
+              borderless
+              textInputStyle={{
+                fontFamily: FontNames.CamptonSemiBold,
+                color: Colors.fontNormal,
+              }}
+            />
+          </View>
         </View>
 
         <Button
@@ -79,15 +84,28 @@ const SignUpPhone: React.FC<ISignUpPhoneProps> = ({}) => {
 
 const styles = StyleSheet.create({
   phoneForm: {
-    display: 'flex',
-    flexDirection: 'row',
+    width: Layout.window.width,
+    height: 50,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    paddingHorizontal: Layout.base,
   },
-  phoneIndicative:{
+  phoneIndicativeView: {
+    width: Layout.window.width * 0.2,
+  },
+  phoneIndicative: {
+    width: Layout.window.width * 0.2,
+  },
+  phoneNumberView: {
     flex: 1,
+    borderColor: "blue",
+    marginLeft: Layout.base - 5,
   },
-  phoneNumber:{
-    
-    
+  phoneNumber: {
+    width: "100%",
+    backgroundColor: Colors.transparent,
   },
   linkButtonPrimary: {
     color: Colors.primary,
