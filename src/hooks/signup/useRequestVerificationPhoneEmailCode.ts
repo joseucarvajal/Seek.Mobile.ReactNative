bfc2dcf911792262ef4187e0959bda5849f97feb
joseucarvajal <joseucarvajal@gmail.com>
2020-10-28 16:43:48 -0500

    phone number in react query request

--- src/hooks/signup/useRequestVerificationPhoneEmailCode.ts
+++ src/hooks/signup/useRequestVerificationPhoneEmailCode.ts
@@ -5,8 +5,8 @@ import { IErrorResponse } from "../../shared";
 
 const getPhoneVerificationCode = async (phoneNumberOrEmail: string) => {
 
-  //const url = `http://192.168.0.101:32700/api/v1/CodeVerification/send/${phoneNumberOrEmail}`;
-  const url = `https://run.mocky.io/v3/4d13c141-982d-427d-8627-e3cdfc74530d`;    
+  const url = `http://192.168.0.101:32700/api/v1/CodeVerification/send/${phoneNumberOrEmail}`;
+  //const url = `https://run.mocky.io/v3/4d13c141-982d-427d-8627-e3cdfc74530d`;    
 
   console.log({ url });
 
@@ -17,7 +17,7 @@ const getPhoneVerificationCode = async (phoneNumberOrEmail: string) => {
 export default function useRequestVerificationPhoneEmailCode(phoneNumberOrEmail: string) {
   const navigation = useNavigation();
 
-  return useQuery<{}, IErrorResponse>(["codeverification", "Validate", phoneNumberOrEmail], getPhoneVerificationCode, {
+  return useQuery<{}, IErrorResponse>([phoneNumberOrEmail], getPhoneVerificationCode, {
     refetchOnWindowFocus: false,
     enabled: false, // turned off by default, manual refetch is needed
     cacheTime: 0,
