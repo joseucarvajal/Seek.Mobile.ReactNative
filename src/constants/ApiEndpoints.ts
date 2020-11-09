c636f37d65df4aac5478455d92acf95d064f94ba
Jorge Puerta <jp@oiga.com>
2020-11-09 09:11:17 -0500

    adjusted some styles

--- src/constants/ApiEndpoints.ts
+++ src/constants/ApiEndpoints.ts
@@ -20,7 +20,7 @@ const ApiEndPoints = {
   },
   profile: {
     getProfile: "https://run.mocky.io/v3/31eea2d7-1488-4a95-8bda-81a7dabfee3f",
-    getAllProfiles: "https://run.mocky.io/v3/77684a79-0430-485f-9282-be4873411063"
+    getAllProfiles: "https://run.mocky.io/v3/77684a79-0430-485f-9282-be4873411063",
     getInterests: "https://run.mocky.io/v3/77684a79-0430-485f-9282-be4873411063"
   },
   myconnections: {
