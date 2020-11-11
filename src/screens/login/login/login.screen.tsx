6fc0c02716ae1f15d5210068b8d67d6db3a824ba
joseucarvajal <joseucarvajal@gmail.com>
2020-11-11 18:08:20 -0500

    expo-auth-session dependence installed and basic view setup

---
+++ src/screens/login/login/login.screen.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { Button, Text, View } from 'react-native';
+import * as AuthSession from 'expo-auth-session';
+import * as WebBrowser from 'expo-web-browser';
+
+WebBrowser.maybeCompleteAuthSession();
+
+const useProxy = true;
+
+const redirectUri = AuthSession.makeRedirectUri({
+  native: 'your.app://redirect',
+  useProxy,
+});
+
+export default function App() {
+  const discovery = AuthSession.useAutoDiscovery('https://demo.identityserver.io');
+  
+  // Create and load an auth request
+  const [request, result, promptAsync] = AuthSession.useAuthRequest(
+    {
+      clientId: 'native.code',
+      redirectUri,
+      scopes: ['openid', 'profile', 'email', 'offline_access'],
+    },
+    discovery
+  );
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Button title="Login!" disabled={!request} onPress={() => promptAsync({ useProxy })} />
+      {result && <Text>{JSON.stringify(result, null, 2)}</Text>}
+    </View>
+  );
+}
